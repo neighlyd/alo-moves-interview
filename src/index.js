622c02c62b2fb3e90ee942d1c672f3387a01b681
Dustin Neighly <neighlyd@gmail.com>
2019-06-13 12:07:37 -0700

    Added snapshot testing and Loading Page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import LoadingPage from './components/LoadingPage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let hasRendered = false;
+
+const renderApp = () => {
+    if (!hasRendered) {
+        ReactDOM.render(<App />, document.getElementById('root'));
+        hasRendered = true;
+    }
+}
+
+ReactDOM.render(<LoadingPage />, document.getElementById('root'));
